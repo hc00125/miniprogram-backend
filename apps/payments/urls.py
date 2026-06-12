21bc16b3b36161c717e16efd213f2edbad2500a4
Hu Chen <164363459+hc00125@users.noreply.github.com>
2026-06-12 14:41:28 +0800

    Add WeChat payment query route

--- apps/payments/urls.py
+++ apps/payments/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('create', views.create),
     path('wechat/miniprogram/create', views.create_wechat_miniprogram),
     path('status/<str:payment_no>', views.status_view),
+    path('wechat/query/<str:payment_no>', views.query_wechat_order),
     path('mock/<str:payment_no>/success', views.mock_success),
     path('wechat/callback', views.wechat_callback),
 ]
