0b9c1be66b8d7c1f34b58f6bb26c866677635db3
Hu Chen <164363459+hc00125@users.noreply.github.com>
2026-07-01 01:10:07 +0800

    Expose uploaded package images in catalog serializer

--- apps/catalog/serializers.py
+++ apps/catalog/serializers.py
@@ -1,6 +1,37 @@
 from rest_framework import serializers
 
-from .models import Addon, Package, PackageGroup, PackageSpec, PlayerType
+from .models import Addon, Package, PackageGroup, PackageImage, PackageSpec, PlayerType
+
+
+def build_absolute_image_url(request, url):
+    if not url:
+        return ''
+    if url.startswith(('http://', 'https://')):
+        return url
+    if request:
+        return request.build_absolute_uri(url)
+    return url
+
+
+def get_image_url(request, image):
+    if not image:
+        return ''
+    return build_absolute_image_url(request, image.get_url())
+
+
+def package_images(obj):
+    images = getattr(obj, 'active_images', None)
+    if images is None:
+        images = obj.images.filter(is_active=True).order_by('image_type', 'sort_order', 'id')
+    return list(images)
+
+
+def image_urls_by_type(obj, request, image_type):
+    return [
+        get_image_url(request, image)
+        for image in package_images(obj)
+        if image.image_type == image_type and get_image_url(request, image)
+    ]
 
 
 class PackageGroupSerializer(serializers.ModelSerializer):
@@ -22,6 +53,12 @@ class PackageSpecSerializer(serializers.ModelSerializer):
 class PackageSerializer(serializers.ModelSerializer):
     group_id = serializers.IntegerField(source='group.id', allow_null=True, read_only=True)
     group_name = serializers.CharField(source='group.name', allow_null=True, read_only=True)
+    cover_url = serializers.SerializerMethodField()
+    image_url = serializers.SerializerMethodField()
+    thumb_url = serializers.SerializerMethodField()
+    picture_url = serializers.SerializerMethodField()
+    gallery_images = serializers.SerializerMethodField()
+    detail_images = serializers.SerializerMethodField()
     specs = serializers.SerializerMethodField()
 
     class Meta:
@@ -33,6 +70,35 @@ class PackageSerializer(serializers.ModelSerializer):
             'gallery_images', 'detail_images', 'detail_text', 'rules_text',
             'sold_count', 'sort_order', 'is_active', 'is_custom', 'specs',
         ]
+
+    @property
+    def request(self):
+        return self.context.get('request')
+
+    def get_cover_url(self, obj):
+        uploaded = image_urls_by_type(obj, self.request, PackageImage.IMAGE_TYPE_COVER)
+        if uploaded:
+            return uploaded[0]
+        return build_absolute_image_url(self.request, obj.cover_url or '')
+
+    def get_image_url(self, obj):
+        return build_absolute_image_url(self.request, obj.image_url or '')
+
+    def get_thumb_url(self, obj):
+        return build_absolute_image_url(self.request, obj.thumb_url or '')
+
+    def get_picture_url(self, obj):
+        return build_absolute_image_url(self.request, obj.picture_url or '')
+
+    def get_gallery_images(self, obj):
+        uploaded = image_urls_by_type(obj, self.request, PackageImage.IMAGE_TYPE_GALLERY)
+        legacy = [build_absolute_image_url(self.request, url) for url in (obj.gallery_images or []) if isinstance(url, str) and url]
+        return uploaded + legacy
+
+    def get_detail_images(self, obj):
+        uploaded = image_urls_by_type(obj, self.request, PackageImage.IMAGE_TYPE_DETAIL)
+        legacy = [build_absolute_image_url(self.request, url) for url in (obj.detail_images or []) if isinstance(url, str) and url]
+        return uploaded + legacy
 
     def get_specs(self, obj):
         specs = getattr(obj, 'active_specs', None)
