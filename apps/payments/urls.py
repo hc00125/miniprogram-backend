f295c2a89e5c3e5b4e767d25cb4738c9cee90fe1
Hu Chen <164363459+hc00125@users.noreply.github.com>
2026-07-10 15:54:06 +0800

    Add virtual payment routes

--- apps/payments/urls.py
+++ apps/payments/urls.py
@@ -5,8 +5,10 @@ from . import views
 urlpatterns = [
     path('create', views.create),
     path('wechat/miniprogram/create', views.create_wechat_miniprogram),
+    path('wechat/virtual/create', views.create_wechat_virtual),
     path('status/<str:payment_no>', views.status_view),
     path('wechat/query/<str:payment_no>', views.query_wechat_order),
+    path('wechat/virtual/query/<str:payment_no>', views.query_wechat_virtual),
     path('mock/<str:payment_no>/success', views.mock_success),
     path('wechat/callback', views.wechat_callback),
 ]
