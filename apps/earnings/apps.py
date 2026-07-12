2061e06ba28e1d29175901b894df3557d82c632f
Hu Chen <164363459+hc00125@users.noreply.github.com>
2026-07-12 11:44:08 +0800

    Load earnings completion signal

--- apps/earnings/apps.py
+++ apps/earnings/apps.py
@@ -5,3 +5,6 @@ class EarningsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.earnings'
     verbose_name = '陪玩收益与钱包'
+
+    def ready(self):
+        from . import signals  # noqa: F401
