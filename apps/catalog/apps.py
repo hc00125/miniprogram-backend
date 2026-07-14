4220caf751e5b5b48d571db5fc4ae1b82ee16248
Hu Chen <164363459+hc00125@users.noreply.github.com>
2026-07-14 19:43:17 +0800

    Load package spec admin registration

--- apps/catalog/apps.py
+++ apps/catalog/apps.py
@@ -5,3 +5,6 @@ class CatalogConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.catalog'
     verbose_name = '套餐管理'
+
+    def ready(self):
+        from . import admin_p0  # noqa: F401
