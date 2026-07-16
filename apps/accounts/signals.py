8f673039f3900562967649867df95870bcc45353
Hu Chen <164363459+hc00125@users.noreply.github.com>
2026-07-16 11:00:32 +0800

    feat(vip): snapshot VIP KOOK room on new orders

--- apps/accounts/signals.py
+++ apps/accounts/signals.py
@@ -4,11 +4,13 @@ from django.dispatch import receiver
 from apps.orders.models import Order
 from apps.payments.models import Refund
 
-from .vip import record_completed_order, record_successful_refund
+from .vip import apply_vip_kook_room_to_order, record_completed_order, record_successful_refund
 
 
 @receiver(post_save, sender=Order)
-def sync_order_consumption(sender, instance, **kwargs):
+def sync_order_consumption(sender, instance, created=False, **kwargs):
+    if created and instance.order_type == Order.ORDER_TYPE_NORMAL:
+        apply_vip_kook_room_to_order(instance)
     record_completed_order(instance)
 
 
