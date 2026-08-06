0b44412807bfaf0bcb819e02e48093c46652bde9
Hu Chen <164363459+hc00125@users.noreply.github.com>
2026-08-06 20:51:26 +0800

    Block iOS payment creation endpoints

--- apps/payments/urls.py
+++ apps/payments/urls.py
@@ -1,15 +1,16 @@
 from django.urls import path
 
 from apps.common.account_guard import require_operational
+from apps.common.purchase_guard import require_purchase_available
 from apps.wallet import views as wallet_views
 
 from . import views
 
 urlpatterns = [
-    path('create', views.create),
-    path('balance/create', require_operational(wallet_views.pay_balance_create)),
-    path('wechat/miniprogram/create', require_operational(views.create_wechat_miniprogram)),
-    path('wechat/virtual/create', require_operational(views.create_wechat_virtual)),
+    path('create', require_purchase_available(views.create)),
+    path('balance/create', require_purchase_available(require_operational(wallet_views.pay_balance_create))),
+    path('wechat/miniprogram/create', require_purchase_available(require_operational(views.create_wechat_miniprogram))),
+    path('wechat/virtual/create', require_purchase_available(require_operational(views.create_wechat_virtual))),
     path('status/<str:payment_no>', views.status_view),
     path('wechat/query/<str:payment_no>', views.query_wechat_order),
     path('wechat/virtual/query/<str:payment_no>', views.query_wechat_virtual),
